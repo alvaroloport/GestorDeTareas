f542717003155f82f2527474a0fa7b3b2e29a508
alvaroloport <alvloport@alu.edu.gva.es>
2026-06-09 15:44:53 +0200

    Integracion base de datos

---
+++ GestorDeTareas/src/main/java/org/example/utils/conexion.java
@@ -0,0 +1,33 @@
+package src.main.java.org.example.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class conexion {
+    public static Connection getConnection(){
+        Connection conexion = null;
+        String base = "gestorTareas";
+        String url = "jdbc:mysql://127.0.0.1:3306/" + base;
+        String usuario = "root";
+        String password = "admin";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        }catch(Exception e){
+            System.out.println("Error" + e);
+        }
+
+        return conexion;
+    }
+
+    public static void main(String[] args) {
+        Connection conexion = getConnection();
+        if(conexion != null){
+            System.out.printf("Conectado");
+        }else{
+            System.out.println("No conectado");
+        }
+    }
+
+}
